2b94aaf8d8d62d7a5248e4a433c38effeb2f9ea6
dunatotatos <contact@dunatotatos.com>
2019-10-20 15:17:20 +0200

    Capture the random hardware reset signals.

--- temperature.py
+++ temperature.py
@@ -1,9 +1,13 @@
 import time
 from w1thermsensor import W1ThermSensor
+from w1thermsensor.errors import ResetValueError
 sensor = W1ThermSensor()
 
 while True:
-    temperature = sensor.get_temperature()
+    try:
+        temperature = sensor.get_temperature()
+    except ResetValueError:
+        print("Temperature sensor sent the reset value.")
     print("The temperature is %s celsius" % temperature)
     time.sleep(1)
 
