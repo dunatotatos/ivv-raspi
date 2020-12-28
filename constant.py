3703b75b930c4288f24c62459df516c4f25353ee
Dunatotatos <contact@dunatotatos.com>
2020-12-28 21:04:06 +0100

    Remove unused configuration.

--- constant.py
+++ constant.py
@@ -1,7 +1,5 @@
 """Application configuration."""
 
-IP_SRV = "192.168.42.13"
-PORT_SRV = 14999
 IP_DST = "192.168.42.42"
 PORT_DST = 14999
 
@@ -13,4 +11,4 @@ BIRD_GPIO = 21
 MOINE_GPIO = 20
 
 # Do not modify below this line.
-URL_DST = "http://{}:{}/".format(IP_SRV, PORT_DST)
+URL_DST = "http://{}:{}/".format(IP_DST, PORT_DST)
